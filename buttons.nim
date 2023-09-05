11458132b8feb618f098309fd5e5c207fcb1dbc2
Toma400 <thomasso_calgori@o2.pl>
2023-09-05 22:55:57 +0200

    Added button-driven colour control

---
+++ buttons.nim
@@ -0,0 +1,7 @@
+import nimfire/colors
+import nimfire/draw
+
+var blueButton*   = newRect((1020, 20), (50, 50), BLUE)
+var greenButton*  = newRect((1080, 20), (50, 50), GREEN)
+var redButton*    = newRect((1140, 20), (50, 50), RED)
+var yellowButton* = newRect((1200, 20), (50, 50), YELLOW)
