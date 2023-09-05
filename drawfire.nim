5ad88923e7dd55f44b259165b1d6d0b301395488
Toma400 <thomasso_calgori@o2.pl>
2023-09-05 22:56:29 +0200

    Added transparency use (& all previous things being integrated, I just spent way too much for one big commit, so commited once per file XD)

--- drawfire.nim
+++ drawfire.nim
@@ -5,50 +5,38 @@ import nimfire/image # required for saving canvas as PNG
 import nimfire/types # required for 'brushDraw' proc
 import nimfire/draw
 import nimfire
+import brushes
+import buttons
 import os
 
-var w    = initWindow((1280, 920), "Drawfire", bg_colour=GREEN)
-var cr   = newRect(pos=(950, 50), size=(50, 50), BLACK)
+var w    = initWindow((1280, 920), "Drawfire", bg_colour=CHOCOLATE)
 var logo = newImage("banner.png", (0, 600))
 
-var canvas = newRect(pos=(0, 0), size=(800, 600), WHITE) # we create canvas Rect
-w.drawRect(canvas)
-w.drawImage(logo)
+# var canvbg = newRect(pos=(0, 0), size=(800, 600), WHITE) # background of canvas (if transparency is set, it allows for white background)
+var canvas = newRect(pos=(0, 0), size=(800, 600), WHITE) # canvas (for drawing)
 
 type
   BRUSHES = enum
-    SLASH, SQUARE, CROSS
+    SLASH, SQUARE, CROSS, CIRCLE
 var brush = 1 # size of a brush
 var brush_type = SLASH
+var brush_rect = newRect(pos=(820, 20), size=(180, 180), IWAI) # background
+var brush_kind = newRect(pos=(910, 110), size=(50, 50), BLACK)  # marker showcasing type of brush & colour
 
-proc brushDrawSlash(w: var Window, pos: (int, int)) = # '\' slash
-  for i in -brush..brush:
-    let x = pos[0] + i
-    let y = pos[1] + i
-    w.fillPos((x, y), cr.colour)
-    setPixel(canvas, (x, y), cr.colour) # we add setPixel here, because this let us
-                                        # notify Rect object about the change done
-                                        # (will be useful for saving it later)
-
-proc brushDrawSquare(w: var Window, pos: (int, int)) = # square
-  for i in -brush..brush:
-    for j in -brush..brush:
-      let x = pos[0] + i
-      let y = pos[1] + j
-      w.fillPos((x, y), cr.colour)
-      setPixel(canvas, (x, y), cr.colour)
-
-proc brushDrawCross(w: var Window, pos: (int, int)) = # cross
-  for i in -brush..brush:
-    let x = pos[0] + i
-    let y = pos[1]
-    w.fillPos((x, y), cr.colour)
-    setPixel(canvas, (x, y), cr.colour)
-  for j in -brush..brush:
-    let x = pos[0]
-    let y = pos[1] + j
-    w.fillPos((x, y), cr.colour)
-    setPixel(canvas, (x, y), cr.colour)
+proc brushDraw(w: var Window, pos: (int, int), b: BRUSHES) =
+    var coords = newSeq[(int, int)]()
+    case b:
+      of SLASH:
+        for c in brushSlash(brush, pos): coords.add(c)
+      of SQUARE:
+        for c in brushSquare(brush, pos): coords.add(c)
+      of CROSS:
+        for c in brushCross(brush, pos): coords.add(c)
+      of CIRCLE:
+        for c in brushCircle(brush, pos): coords.add(c)
+    for coord in coords:
+      w.fillPos(coord, brush_kind.colour)
+      setPixel(canvas, coord, brush_kind.colour)
 
 proc save(r: Rect) =
     for i in 1..100_000:
@@ -56,55 +44,83 @@ proc save(r: Rect) =
         saveImage(r.toImage(), fmt"image{i}.png")
         break
 
-
-while w.tick():
-  w.drawRect(cr) # draws Rect showing current colour
-
-  if w.getKeyPressed(KEY.B):
-    cr.setColour(BLACK)
-  elif w.getKeyPressed(KEY.G):
-    cr.setColour(GREEN)
-  elif w.getKeyPressed(KEY.R):
-    cr.setColour(RED)
-  elif w.getKeyPressed(KEY.W):
-    cr.setColour(WHITE)
-  elif w.getKeyPressed(KEY.L):
-    cr.setColour(BLUE)
-  elif w.getKeyPressed(KEY.Y):
-    cr.setColour(YELLOW)
-  elif w.getKeyPressed(KEY.P):
-    cr.setColour(PURPLE)
-  elif w.getKeyPressed(KEY.Q):
+proc clear() =
     w.clear()
     clearPixels(canvas)
-    w.drawRect(canvas)                    # we add redrawing canvas here, since this makes our canvas empty again
-    w.drawImage(logo)
+    # w.drawRect(canvbg)
+    w.drawRect(canvas)
+    w.drawRect(brush_rect)
+    block Buttons:
+      w.drawRect(blueButton)
+      w.drawRect(greenButton)
+      w.drawRect(redButton)
+      w.drawRect(yellowButton)
+    block Images:
+      w.drawImage(logo)
+
+clear()
+
+while w.tick():
+  brushDraw(w, brush_kind.pos, brush_type)
+
+  if w.getKeyPressed(KEY.B):
+    brush_kind.setColour(BLACK)
+  elif w.getKeyPressed(KEY.G):
+    brush_kind.setColour(GREEN)
+  elif w.getKeyPressed(KEY.R):
+    brush_kind.setColour(RED)
+  elif w.getKeyPressed(KEY.W):
+    brush_kind.setColour(WHITE)
+  elif w.getKeyPressed(KEY.L):
+    brush_kind.setColour(BLUE)
+  elif w.getKeyPressed(KEY.Y):
+    brush_kind.setColour(YELLOW)
+  elif w.getKeyPressed(KEY.P):
+    brush_kind.setColour(PURPLE)
+  elif w.getKeyPressed(KEY.Q):
+    clear()
   elif w.getKeyPressed(KEY.UP):
+    w.drawRect(brush_rect)
     brush += 1
   elif w.getKeyPressed(KEY.DOWN):
+    w.drawRect(brush_rect)
     if brush > 1:
       brush -= 1
   elif w.getKeyPressed(KEY.LEFT):
+    w.drawRect(brush_rect)
     case brush_type:
       of SLASH:  brush_type = SQUARE
       of SQUARE: brush_type = CROSS
-      of CROSS:  brush_type = SLASH
+      of CROSS:  brush_type = CIRCLE
+      of CIRCLE: brush_type = SLASH
   elif w.getKeyPressed(KEY.RIGHT):
+    w.drawRect(brush_rect)
     case brush_type:
-      of SLASH:  brush_type = CROSS
+      of SLASH:  brush_type = CIRCLE
       of SQUARE: brush_type = SLASH
       of CROSS:  brush_type = SQUARE
+      of CIRCLE: brush_type = CROSS
   elif w.getKeyPressed(KEY.TAB):
     save(canvas)
+  elif w.getKeyPressed(KEY.T):
+    if canvas.colour == toRGBX(0, 0, 0, 0): setColour(canvas, WHITE)
+    else:                                   setColour(canvas, toRGBX(0, 0, 0, 0))
+    clear()
 
   # drawing with mouse
   if w.getMousePressed(LEFT):
     var pos = w.getMousePos()
     if collide(canvas, pos): # and we also add condition for drawing to do drawing only when it is within canvas
-      case brush_type:
-        of SLASH:  brushDrawSlash(w, pos)
-        of SQUARE: brushDrawSquare(w, pos)
-        of CROSS:  brushDrawCross(w, pos)
+      brushDraw(w, pos, brush_type)
+    else:
+      if collide(blueButton, pos):
+        brush_kind.setColour(BLUE)
+      elif collide(greenButton, pos):
+        brush_kind.setColour(GREEN)
+      elif collide(redButton, pos):
+        brush_kind.setColour(RED)
+      elif collide(yellowButton, pos):
+        brush_kind.setColour(YELLOW)
 
   w.update(manual=true)
 
